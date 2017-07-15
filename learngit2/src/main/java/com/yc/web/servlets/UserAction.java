fd5870a7033f75ddfa9d0e3b744ff0677d42d50d
Gao <1084556678@qq.com>
2017-07-15 20:33:37 +0800

    基本的内容，登录和显示

---
+++ learngit2/src/main/java/com/yc/web/servlets/UserAction.java
@@ -0,0 +1,43 @@
+package com.yc.web.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserAction
+ */
+public class UserAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UserAction() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- learngit2/src/main/webapp/index.jsp
+++ learngit2/src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>登录</h2>
+<form action="userAction" method="post">
+	编号：<input type="text" name="id">
+	姓名：<input type="text" name="name">
+	<input type="submit" value="登录">
+</form>
 </body>
 </html>
